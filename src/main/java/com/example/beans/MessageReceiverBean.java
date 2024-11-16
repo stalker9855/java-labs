992ddce816028ddfa1bb16d8ecf69f087df8651a
stalker9855 <andrey.l.evseev@gmail.com>
2024-11-16 19:47:19 +0200

    lr10

---
+++ src/main/java/com/example/beans/MessageReceiverBean.java
@@ -0,0 +1,47 @@
+package com.example.beans;
+
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSConsumer;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Queue;
+import jakarta.jms.TextMessage;
+
+@Named
+@ApplicationScoped
+public class MessageReceiverBean {
+
+  @Resource(lookup = "jms/testQueue")
+  private Queue queue;
+
+  @Resource
+  private ConnectionFactory connectionFactory;
+
+  private String lastMessage = "No notifications yet.";
+
+  public void receiveMessage() {
+    try (JMSContext context = connectionFactory.createContext()) {
+      JMSConsumer consumer = context.createConsumer(queue);
+      Message message = consumer.receive(1000);
+      if (message instanceof TextMessage) {
+        lastMessage = ((TextMessage) message).getText();
+      } else {
+        lastMessage = "No notifications.";
+      }
+    } catch (JMSException e) {
+      e.printStackTrace();
+    }
+  }
+
+public String getLastMessage() {
+	return lastMessage;
+}
+
+}
+
+
+
---
+++ src/main/java/com/example/beans/MessageSenderBean.java
@@ -0,0 +1,51 @@
+package com.example.beans;
+
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSProducer;
+import jakarta.jms.Queue;
+
+/**
+ * 
+ */
+@Named
+@RequestScoped
+public class MessageSenderBean {
+
+  @Resource(lookup = "jms/testQueue")
+  private Queue queue;
+
+  @Resource
+  private ConnectionFactory connectionFactory;
+
+  private String filter;
+  private String messageText;
+
+  public void sendMessage() {
+    try (JMSContext context = connectionFactory.createContext()) {
+      JMSProducer producer = context.createProducer();
+      producer.setProperty("filter", filter);
+      producer.send(queue, messageText);
+    }
+  }
+
+  public String getFilter() {
+    return filter;
+  }
+
+  public void setFilter(String filter) {
+    this.filter = filter;
+  }
+
+  public String getMessageText() {
+    return messageText;
+  }
+
+  public void setMessageText(String messageText) {
+    this.messageText = messageText;
+  }
+
+}
