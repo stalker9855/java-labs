3446cdd6f6ac20dbf2be19d5e2bee88d61f4b45a
stalker9855 <andrey.l.evseev@gmail.com>
2024-11-17 10:51:38 +0200

    lr11

--- src/main/java/com/example/controllers/BookController.java
+++ src/main/java/com/example/controllers/BookController.java
@@ -120,8 +120,6 @@ public class BookController {
       return Response.status(Response.Status.NOT_FOUND).entity("Book not found").build();
     }
 
-    System.out.println("AUTHOR ID" + book.getAuthor().getId());
-    System.out.println(book.getId());
     bookService.deleteBook(id);
     return Response.ok("redirect:books").build();
   }
